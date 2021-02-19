6f67078df67a712d6aef4cd9754f51fa5ded1a6f
Ubuntu <ubuntu@ip-172-31-14-99.ap-northeast-1.compute.internal>
2021-02-19 10:44:17 +0000

    initial push

---
+++ test_app_v1/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to ("/users")
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:password)
+  end
+end
